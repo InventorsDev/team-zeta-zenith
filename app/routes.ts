8a321b2c08b037463609e4dd38a0363a043e48c3
Samuel Ogboye <ogboyesam@gmail.com>
2025-10-03 23:11:37 +0100

    feat: update dashboard route to use layout component with nested routes

--- app/routes.ts
+++ app/routes.ts
@@ -5,5 +5,11 @@ export default [
   route("onboarding", "routes/onboarding.tsx"),
   route("signup", "routes/signup.tsx"),
   route("login", "routes/login.tsx"),
-  route("dashboard", "routes/dashboard.tsx")
+  route("dashboard", "routes/dashboard-layout.tsx", [
+    index("routes/dashboard/overview.tsx"),
+    route("tickets", "routes/dashboard/tickets.tsx"),
+    route("analytics", "routes/dashboard/analytics.tsx"),
+    route("integrations", "routes/dashboard/integrations.tsx"),
+    route("settings", "routes/dashboard/settings.tsx"),
+  ]),
 ] satisfies RouteConfig;
